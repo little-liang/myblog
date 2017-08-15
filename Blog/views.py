eb4f9f90c1aadd2411f7d615c48068fae6b7ad63
liangyanlong <liangyanlong@hydsoft.com>
2017-08-15 09:36:09 +0800

    admin1 开始显示文章

--- Blog/views.py
+++ Blog/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
 
 # Create your views here.
+from Blog.models import *
+import markdown
 def index(request):
+
     return render(request, 'index.html')
 
 def admin1(request):
-    return render(request, 'admin1.html')
+
+    article_list = Article.objects.all()
+    print(article_list)
+    return render(request, 'admin1.html', context={'article_list': article_list})

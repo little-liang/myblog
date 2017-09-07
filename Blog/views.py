885a201ed5af51badd102baa849d5b1c38fa1353
liangyanlong <liangyanlong@hydsoft.com>
2017-09-07 09:28:01 +0800

    调整了管理界面的 显示

--- Blog/views.py
+++ Blog/views.py
@@ -1,17 +1,19 @@
+
 from django.shortcuts import render, HttpResponse, redirect
-import json
+import json,datetime
 from django.views.decorators.csrf import csrf_exempt
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
-
+# coding:utf-8
 # Create your views here.
 from Blog.models import *
+from django.utils import timezone
 def index(request):
 
     return render(request, 'index.html')
 
 def article_admin(request):
-    every_page_num = 5  # 每页显示的记录数
-    p = Paginator(Article.objects.all(), every_page_num)
+    every_page_num = 5  # 每页显示的记录数  #不加 order by 会有警告信息
+    p = Paginator(Article.objects.all().order_by('-publish_time',), every_page_num)
 
     ##前端点击的page,第几页
     page = request.GET.get('page')
@@ -29,6 +31,11 @@ def article_admin(request):
     except EmptyPage:
         # If page is out of range (e.g. 9999), deliver last page of results.
         contacts = p.page(p.num_pages)
+
+
+    for line in contacts:
+        line.publish_time = datetime.datetime.strftime(line.publish_time, "%Y-%m-%d %H:%M:%S")
+
 
     return render(request, 'article_admin.html', context={
         'contacts': contacts, 'every_page_num': every_page_num,

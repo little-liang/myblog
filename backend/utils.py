a7ae141352d35db98e6114bca4577d7ba31112bd
Administrator <740658054@qq.com>
2017-09-13 20:28:12 +0800

    结构变化， 分成前端 和 后端

---
+++ backend/utils.py
@@ -0,0 +1,28 @@
+##多个页面提交
+##提交有 增删改查 （命令不行一样）
+
+
+##参数  表名
+##create  数据
+##update  数据
+## delete 数据data json
+
+class crud(object):
+    def __init__(self, command):
+        self.command = command
+
+    def create(self):
+        pass
+
+    def update(self):
+        pass
+
+    def delete(self):
+        pass
+
+    def search(self):
+        pass
+
+
+#第二个 就是 page 分页 这个函数化 就行了
+##参数 表名 显示的数量

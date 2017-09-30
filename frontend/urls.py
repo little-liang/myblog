fb9ba5a9ccd7b295a2aa1439f99e2be8dd5a5e01
liangyanlong <liangyanlong@hydsoft.com>
2017-09-30 15:08:04 +0800

    访问 article_id 可以了

--- frontend/urls.py
+++ frontend/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import url
-from .views import *
+from frontend import views
 
 urlpatterns = [
-    url(r'^category.html$', category, name='category'),
+    url(r'^category.html$', views.category, name='category'),
+    url(r'^article/(?P<article_id>\d+).html$', views.test),
 ]
--- frontend/views.py
+++ frontend/views.py
@@ -8,7 +8,14 @@ def index(request):
     article_info = Article.objects.all().order_by('publish_time')
     for article in article_info:
         article.publish_time = datetime.datetime.strftime(article.publish_time, "%Y-%m-%d")
+        ##文章id伪装
+        article.id = deal_id(article.id)
     return render(request, 'frontend/index.html', context={'article_info': article_info})
+
+def deal_id(a_id):
+    return a_id + 87521
+
+
 
 
 def article(request):
@@ -31,3 +38,11 @@ def links(request):
 
 def UpgradeBrowser(request):
     return render(request, 'frontend/UpgradeBrowser.html')
+
+
+def test(request, *args, **kwargs):
+    article_id = kwargs['article_id']
+    article_id = int(article_id) - 87521
+    article_info = Article.objects.get(id=article_id)
+
+    return render(request, 'frontend/article.html', context={'article_info': article_info})
